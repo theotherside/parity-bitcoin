3d931576bf03cda8063d0a0dd5e41c3969579621
NikVolf <nikvolf@gmail.com>
2016-12-08 13:32:00 +0100

    actualy test it

--- db/src/unspent_transaction.rs
+++ db/src/unspent_transaction.rs
@@ -60,12 +60,14 @@ impl UnspentUpdate {
 	}
 }
 
+#[cfg(test)]
 mod tests {
 
 	use super::*;
 	use primitives::H256;
 	use chain;
 
+	#[test]
 	fn from_into() {
 		let key = UnspentTransaction::new(
 			"75c7985c5dc0ca8cc56ac3618c57f0060b0c2cf9ad840ed31b7e835807fcb2fb".into(),
@@ -76,6 +78,7 @@ mod tests {
 		assert_eq!(output.transaction, H256::from("d6a0dd90afb9540bc0b8e59c861c3fd1f2313eb617663a07688e37717a51a367"));
 	}
 
+	#[test]
 	fn update() {
 		let t: chain::Transaction = "0100000001a6b97044d03da79c005b20ea9c0e1a6d9dc12d9f7b91a5911c9030a439eed8f5000000004948304502206e21798a42fae0e854281abd38bacd1aeed3ee3738d9e1446618c4571d1090db022100e2ac980643b0b82c0e88ffdfec6b64e3e6ba35e7ba5fdd7d5d6cc8d25c6b241501ffffffff0100f2052a010000001976a914404371705fa9bd789a2fcd52d2c580b65d35549d88ac00000000".into();
 
@@ -85,6 +88,7 @@ mod tests {
 		assert_eq!(1, update.new.len(), "There should be one unspent transaction to add");
 	}
 
+	#[test]
 	fn update_remove() {
 		let t: chain::Transaction = "0100000001a6b97044d03da79c005b20ea9c0e1a6d9dc12d9f7b91a5911c9030a439eed8f5000000004948304502206e21798a42fae0e854281abd38bacd1aeed3ee3738d9e1446618c4571d1090db022100e2ac980643b0b82c0e88ffdfec6b64e3e6ba35e7ba5fdd7d5d6cc8d25c6b241501ffffffff0100f2052a010000001976a914404371705fa9bd789a2fcd52d2c580b65d35549d88ac00000000".into();
 
