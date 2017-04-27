44b4bf089c8efb36aeecbe96d85f4eaf94623a09
Travis CI User <travis@example.org>
2017-04-27 14:40:36 +0000

    Update documentation

---
+++ search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["pbtc"] = {"doc":"Parity bitcoin client.","items":[[17,"APP_INFO","pbtc","",null,null],[17,"PROTOCOL_VERSION","","",null,null],[17,"PROTOCOL_MINIMUM","","",null,null],[17,"USER_AGENT","","",null,null],[17,"REGTEST_USER_AGENT","","",null,null],[17,"LOG_INFO","","",null,null]],"paths":[]};
+initSearch(searchIndex);
